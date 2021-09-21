c63e5198f5e1c8345e0f13c33c61c228cbca38ad
JahnieTurner <80420943+JahnieTurner@users.noreply.github.com>
2021-09-21 19:56:38 -0400

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,12 +1,31 @@
+p1name = localStorage.getItem("player1name")
+p2name = localStorage.getItem("player2name")
+
+p1score = 0
+p2score = 0
+
+Q_turn = "p1"
+A_turn = "p2"
+
+document.getElementById("p1name").innerHTML = p1name + ":"
+document.getElementById("p2name").innerHTML = p2name + ":"
+
+document.getElementById("p1score").innerHTML = p1score
+document.getElementById("p2score").innerHTML = p2score
+
+document.getElementById("player_question").innerHTML = "Question Turn :" + p1name
+document.getElementById("player_answer").innerHTML = "Answer Turn :" + p2name
+
+
 function Send() {
-    number1 = document.gentElementById("number1").value;
-    number2 = document.gentElementById("number1").value;
-    actual_answer = parseInt(number1) * parseInt(number1);
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
 
     question_number = "<h4>" + number1 + " X " + number2 + "</h4>";
     input = "<br>Answer : <input type='text' id='input_check_box'>";
     check_button = "<br><br><button class='btn btn-info' onclick='Check()'>Check</button>";
-    row = question_number + input_box + check_button;
+    row = question_number + input + check_button;
 
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
